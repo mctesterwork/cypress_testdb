850c6e60228750545b160ddff9f3d2f0ce86be77
Miguel C <mctesterwork@gmail.com>
2023-10-13 11:07:15 -0500

    Added expect for first connection test

--- cypress/e2e/DB integration/testConnect.cy.js
+++ cypress/e2e/DB integration/testConnect.cy.js
@@ -2,7 +2,8 @@ describe('Connection tests for sakila movie database', () => {
     it('Selects all rows from movie table', async () => {
         cy.task('queryDb', 
         `SELECT * FROM film`).then(films => {
-            console.log(films);
+            console.log(films)
+            expect(films[0].title).to.equal('ACADEMY DINOSAUR')
         });
     })
 })
